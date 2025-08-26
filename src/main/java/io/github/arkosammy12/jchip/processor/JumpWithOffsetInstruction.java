49dadebfbb8e2fab9b99f384880c60a2234faede
ArkoSammy12 <jamesvillarreal12@gmail.com>
2025-08-26 00:51:37 -0500

    Add missing quirk comment

--- src/main/java/io/github/arkosammy12/jchip/processor/JumpWithOffsetInstruction.java
+++ src/main/java/io/github/arkosammy12/jchip/processor/JumpWithOffsetInstruction.java
@@ -11,6 +11,7 @@ public class JumpWithOffsetInstruction extends Instruction {
     @Override
     public void execute(Emulator emulator) {
         int memoryAddress = this.getMemoryAddress();
+        // Using v0 as the offset instead of jumping to XNN + VX is a quirk. COSMAC CHIP-8
         int v0 = emulator.getProcessor().getRegisterValue(0x0);
         int jumpAddress = memoryAddress + v0;
         emulator.getProcessor().setProgramCounter(jumpAddress);
