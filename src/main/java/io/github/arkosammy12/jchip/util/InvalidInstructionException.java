ba059dbdd453c4d05fddec6bae72d6740f9af064
ArkoSammy12 <jamesvillarreal12@gmail.com>
2025-09-06 17:22:02 -0500

    e

--- src/main/java/io/github/arkosammy12/jchip/util/InvalidInstructionException.java
+++ src/main/java/io/github/arkosammy12/jchip/util/InvalidInstructionException.java
@@ -3,7 +3,7 @@ package io.github.arkosammy12.jchip.util;
 public class InvalidInstructionException extends Exception {
 
     public InvalidInstructionException(int firstNibble, int secondNibble, int secondByte, ConsoleVariant consoleVariant) {
-        super("Instruction " + ((((firstNibble << 4) | secondNibble) << 8) | secondByte) + " is invalid on the " + consoleVariant.getDisplayName() + " variant!");
+        super("Instruction " + Integer.toHexString(((((firstNibble << 4) | secondNibble) << 8) | secondByte)) + " is invalid on the " + consoleVariant.getDisplayName() + " variant!");
     }
 
 }
